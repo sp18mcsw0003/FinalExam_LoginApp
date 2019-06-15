f1bb2002aaa90504b2d6daf98113c4f42caf4115
Azhar Ahmed <azhar@horizontech.biz>
2019-06-15 17:01:14 +0500

    Starting

---
+++ FinalExam_LoginApp/android/app/src/main/java/com/finalexam_loginapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.finalexam_loginapp;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "FinalExam_LoginApp";
+    }
+}
